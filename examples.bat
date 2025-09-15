f34387a11c91c5e587471228418c804fc06e107a
Stif <stiflabarque@fisheye.eu>
2025-09-15 13:54:59 +0200

    remove verbose flag from example

--- examples.bat
+++ examples.bat
@@ -3,5 +3,4 @@ python readbloodresultspdf.py ^
 -p Creatinine -p CRP -p "Vancomycine cont. infuus" -p "Gamma GT" -p "AST (OT)" -p "ALT (PT)" ^
 -e 2025-05-16,g,--,Ongeval ^
 -e "2025-06-23,b,--,Debridement en spiertransfer" ^
--e "2025-08-07,r,--,Opstart Vanco" ^
--v
+-e "2025-08-07,r,--,Opstart Vanco"
