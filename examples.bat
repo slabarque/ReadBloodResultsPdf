eb85609f4b3e7a10e5d0de91a961b94faccb7f7c
Stif <stiflabarque@fisheye.eu>
2025-10-05 15:49:20 +0200

    add net event

--- examples.bat
+++ examples.bat
@@ -1,6 +1,8 @@
 rem python readbloodresultspdf.py -h
+rem -p Leukocyten -p Lymfocyten -p "Lymfocyten abs" -p Normoblasten -p Trombocyten ^
 python readbloodresultspdf.py ^
 -p Creatinine -p CRP -p "Vancomycine cont. infuus" -p "Gamma GT" -p "AST (OT)" -p "ALT (PT)" ^
 -e 2025-05-16,g,--,Ongeval ^
 -e "2025-06-23,b,--,Debridement en spiertransfer" ^
--e "2025-08-07,r,--,Opstart Vanco"
+-e "2025-08-07,r,--,Opstart Vanco" ^
+-e "2025-09-19,y,--,?"
