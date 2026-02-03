1df0f4c75356cda92bc1ba3cb9d1fe732ce968fd
Stif <stif.labarque@gmail.com>
2026-02-03 12:46:59 +0100

    add new event and debug launch profile

--- examples.bat
+++ examples.bat
@@ -1,9 +1,21 @@
 rem py readbloodresultspdf.py -h
-rem -p Leukocyten -p Lymfocyten -p "Lymfocyten abs" -p Normoblasten -p Trombocyten ^
+rem -p Leukocyten -p "Neutrofiele granulocyten" -p "Lymfocyten abs" -p Normoblasten -p Trombocyten ^
 
+rem Opvolging Vancomycine en nier- en leverfuncties
+@REM py readbloodresultspdf.py ^
+@REM -p Creatinine -p CRP -p "Vancomycine cont. infuus" -p "Gamma GT" -p "AST (OT)" -p "ALT (PT)" ^
+@REM -e 2025-05-16,g,--,Ongeval ^
+@REM -e "2025-06-23,b,--,Debridement en spiertransfer" ^
+@REM -e "2025-08-07,r,--,Opstart Vanco" ^
+@REM -e "2025-09-19,y,--,?" ^
+@REM -e "2026-01-15,g,--,Infectie"
+
+
+rem Infectie indicatoren
 py readbloodresultspdf.py ^
--p Creatinine -p CRP -p "Vancomycine cont. infuus" -p "Gamma GT" -p "AST (OT)" -p "ALT (PT)" ^
+-p CRP -p Leukocyten -p "Neutrofiele granulocyten" ^
 -e 2025-05-16,g,--,Ongeval ^
 -e "2025-06-23,b,--,Debridement en spiertransfer" ^
 -e "2025-08-07,r,--,Opstart Vanco" ^
--e "2025-09-19,y,--,?"
+-e "2025-09-19,y,--,?" ^
+-e "2026-01-15,g,--,Infectie"
